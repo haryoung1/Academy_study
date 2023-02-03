c1efb956de3ef4de55bf7c2a616c294cc5adca71
haryoung1 <krrabbit9494@gmail.com>
2023-02-03 17:24:02 +0900

    last

---
+++ source/05_React/ch05_functioncomponent2/src/App.js
@@ -0,0 +1,19 @@
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+
+
+
+
+
+
+
+function App() {
+  return (
+    <Subject></Subject>
+    <TOC></TOC>
+    <Control></Control>
+
+  );
+}
+
+export default App;
