16ba3c6308cb99245ce8dc0d0b822dce482d4105
haryoung1 <krrabbit9494@gmail.com>
2023-04-07 17:01:34 +0900

    mvcBoard

---
+++ source/08_Spring/ch11_mvcboard/src/main/java/com/lec/ch11/controller/MvcBoardController.java
@@ -0,0 +1,85 @@
+package com.lec.ch11.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lec.ch11.dto.BoardDto;
+import com.lec.ch11.service.*;
+
+@Controller
+@RequestMapping("mvcBoard") // 공통 요청경로
+public class MvcBoardController {
+	private Service bservice;
+
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public String list(String pageNum, Model model) {
+		// mvcBoard / list.do
+		// mvcBoard / list.do?pageNum=10
+		model.addAttribute("pageNum", pageNum);
+		bservice = new BListService();
+		bservice.execute(model);
+		return "mvcBoard/list";
+	}
+
+	@RequestMapping(value = "list", method = RequestMethod.POST)
+	public String list_post(String pageNum, Model model) {
+		// mvcBoard / list.do
+		// mvcBoard / list.do?pageNum=10
+		model.addAttribute("pageNum", pageNum);
+		bservice = new BListService();
+		bservice.execute(model);
+		return "mvcBoard/list";
+	}
+
+	@RequestMapping(value = "write", method = RequestMethod.GET)
+	public String write() {
+		return "mvcBoard/write";
+	}
+
+	@RequestMapping(value = "write", method = RequestMethod.POST)
+	public String write(@ModelAttribute("bDto") BoardDto boardDto, HttpServletRequest request, Model model) {
+		// request : 서비스단에서 ip 추출 용도로
+		model.addAttribute("request", request);
+		bservice = new BWriteService();
+		bservice.execute(model);
+		return "forward:list.do";
+	}
+
+	@RequestMapping(value = "content", method = RequestMethod.GET)
+	public String content(int bid, Model model) {
+		model.addAttribute("bid", bid);
+		bservice = new BContentService();
+		bservice.execute(model);
+		return "mvcBoard/content";
+	}
+
+	@RequestMapping(value = "content", method = RequestMethod.POST)
+	public String content_post(int bid, Model model) {
+		model.addAttribute("bid", bid);
+		bservice = new BContentService();
+		bservice.execute(model);
+		return "mvcBoard/content";
+	}
+
+	@RequestMapping(value = "modify", method = RequestMethod.GET)
+	public String modify(int bid, Model model) {
+		model.addAttribute("bid", bid);
+		bservice = new BModifyReplyViewService();
+		bservice.execute(model);
+		return "mvcBoard/modify";
+
+	}
+
+	@RequestMapping(value = "modify", method = RequestMethod.POST)
+	public String modify(BoardDto boardDto, HttpServletRequest request, Model model) {
+		model.addAttribute("request", request);
+		bservice = new BModifyService();
+		bservice.execute(model);
+		return "forward:content.do";
+	}
+}
